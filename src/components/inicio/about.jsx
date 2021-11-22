28277f2ca952ceed325c3abf642a168afbefb3c1
Dluna.dev <69652080+Dlunadev7@users.noreply.github.com>
2021-11-22 14:12:33 -0300

    Update about.jsx

--- src/components/inicio/about.jsx
+++ src/components/inicio/about.jsx
@@ -31,7 +31,7 @@ function About() {
                             Somos una compañía desarrolladora inmobiliaria que nació en 1992. Contamos con una amplia experiencia en el mercado inmobiliario argentino. Hemos participado en el desarrollo de más de 1.000.000 m2 y administrado proyectos por más de U$S 1.000 millones, entre los más destacados: el Hotel Hilton de Puerto Madero, Torre Bouchard Plaza, Edificio Samsung, Edificios River View, Palacio Paz XXI, Edificio La Rural entre otros.
                         </p>
                         <Link to="nosotros">
-                            <ButtonAnimation text="Conocemos" style={{ marginTop: '48px' }} />
+                            <ButtonAnimation text="Conocenos" style={{ marginTop: '48px' }} />
                         </Link>
                     </div>
                 </div>
